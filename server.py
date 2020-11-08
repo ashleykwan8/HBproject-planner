b63d855d13c895af59aa0a2c9e846915516b1cac
Ashley Kwan <ashleykwan888@gmail.com>
2020-11-08 21:02:03 +0000

    added entry,account seed data, started hompage, login and create user HTML, started server.py

--- server.py
+++ server.py
@@ -4,14 +4,59 @@ from flask import Flask
 from flask import (Flask, render_template, request, flash, session,
                    redirect)
 from jinja2 import StrictUndefined
+from model import connect_to_db
+import crud
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
+@app.route('/')
+def homepage():
+    """View homepage"""
+
+    return render_template("homepage.html")
+
+@app.route('/users', methods=["POST"])
+def register_user(): 
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = crud.get_user_by_email(email)
+
+    if user:
+        flash('Email already exists!')
+    else:
+        crud.create_user(email, password)
+        flash('Account created successfully!')
+
+    return redirect('/')
+
+@app.route('/users')
+def user_login():
+
+    email = request.form.get('login-username')
+    password = request.form.get('login-password')
+
+    user= crud.get_user_by_email(email)
+    login_password = crud.get_user_by_password(password)
+
+    session['user'] = User.user_id
+
+    if user:
+        flash('Logged in!')
+
+    else:
+        flash('Need to create an account!')
+
+    return redirect('/')
 
 
+if __name__ == '__main__':
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)
 
 
 
